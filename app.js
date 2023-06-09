318d07e8a57ad758415b39bc3366dd6f79b3e3ee
warsiKamran <kamranwarsi.kw@gmail.com>
2023-06-09 14:53:42 +0530

    added css and completed the project

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
     // res.sendFile(__dirname + "index.html");
